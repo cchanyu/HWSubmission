0354c02799b405b4b51ba4ffecba65fc0e889a26
Cchanyu <Chanyu96@gmail.com>
2021-12-07 00:08:27 -0500

    fixed items update after deletion triggered

--- src/components/Saved.js
+++ src/components/Saved.js
@@ -1,39 +1,52 @@
 import React from 'react';
 import '../css/Saved.css'
 
-const Saved = (props) => {
-    const { saved } = props;
-    let saveditems =  { ...localStorage };
-    const toURL = (props) => { window.open(props) }
-    const handleDelete = (e) => {
-        localStorage.removeItem(e);
-    }
+class Saved extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            saveditems: {...localStorage},
+            count: Object.keys({...localStorage}).length
+        }
+      }
 
-    return(
-        <div className="saved">
-            <div className="searched">Saved GIFs</div>
-            <div className="saved--list">
-                {Object.keys(saveditems).map((saved, index) => {
-                    const url = `https://media3.giphy.com/media/${saved}/giphy.gif`
-                    return (
-                        <div className="saved--box" key={index}>
-                            <img
-                                key={saved}
-                                className="saved--card" 
-                                src={url}
-                                alt={saved}
-                                onClick={() => toURL(url)}
-                            />
-                            <div className="gif--cred">
-                                <div className="gif--id">ID: {saved}</div>
-                                <button className="saveit icon del" onClick={() => handleDelete(saved)}>Delete</button>
+    render() {
+        const { saveditems, count } = this.state;
+        const toURL = (props) => {window.open(props)}
+        const handleDelete = (e) => {
+            localStorage.removeItem(e);
+            this.setState({
+                saveditems: {...localStorage},
+                count: Object.keys({...localStorage}).length
+            })
+        }
+
+        return(
+            <div className="saved">
+                <div className="searched">Saved GIFs: {count}</div>
+                <div className="saved--list">
+                    {Object.keys(saveditems).map((saved, index) => {
+                        const url = `https://media3.giphy.com/media/${saved}/giphy.gif`
+                        return (
+                            <div className="saved--box" key={index}>
+                                <img
+                                    key={saved}
+                                    className="saved--card" 
+                                    src={url}
+                                    alt={saved}
+                                    onClick={() => toURL(url)}
+                                />
+                                <div className="gif--cred">
+                                    <div className="gif--id">ID: {saved}</div>
+                                    <button className="saveit icon del" onClick={handleDelete.bind(this, saved)}>Delete</button>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Saved;
